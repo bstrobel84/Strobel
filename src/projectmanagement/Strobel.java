de866d11db49edb77ec7becae88123cddaa8492a
bstrobel84@gmail.com <>
2019-02-06 12:30:01 -0500

    this is the first commit

---
+++ src/projectmanagement/Strobel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectmanagement;
+
+/**
+ *
+ * @author bstro
+ */
+public class Strobel {
+    public static void main(String[] args) {
+        Strobel();
+    }
+    public static void Strobel(){
+        System.out.println("Brandon Strobel");
+    }
+}
